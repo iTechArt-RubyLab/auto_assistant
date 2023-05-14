3d98299258d0b1bb27ee1e6dadc3d566f120e8e2
ShalaevPavel <lohgewt123@yandex.by>
2023-05-14 22:16:12 +0300

    checkbox for requesting service ownership works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
+
+
   def set_locale
     I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
     session[:locale] = I18n.locale
@@ -8,6 +10,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:service_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:request_service_owner])
   end
 
 end
