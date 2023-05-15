35884f9176acdde6fccd4609689b499cff257d22
ShalaevPavel <lohgewt123@yandex.by>
2023-05-15 13:03:12 +0300

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,12 @@
 class UserMailer < ApplicationMailer
+
+  def log_in_gritting
+    mail(
+      to: "pashasha1212@mail.ru",
+      subject: "You logged in successfully!"
+
+    )
+  end
   def expiry_warning(user)
     @user = user
     mail(to: user.email, subject: "Expiry Warning")
