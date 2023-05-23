b8e72a135febd80dceaaa5d61d8a9c34059ccd23
ShalaevPavel <lohgewt123@yandex.by>
2023-05-23 18:58:15 +0300

    all buisness logic is valid except for log update in work controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -5,10 +5,27 @@ class WorksController < InheritedResources::Base
     super
   end
 
+  def create
+    @work = Work.new(work_params)
+    car = Car.find(params[:work][:car_id])
+    if car.log
+      log = car.log
+      # log.params[:work][:service_work] = params[:work][:next_appointment]
+      log.water_removal = params[:work][:next_appointment]
+
+      if @work.save
+        log.save
+      end
+    else
+      @work.save
+    end
+    super
+  end
+
   private
 
-    def work_params
-      params.require(:work).permit(:car_id, :service_work, :next_appointment)
-    end
+  def work_params
+    params.require(:work).permit(:car_id, :service_work, :next_appointment, :service_name)
+  end
 
 end
